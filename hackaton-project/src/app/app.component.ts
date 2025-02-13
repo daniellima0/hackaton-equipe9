126a5deaf00611e1c868f57ce75df830f06dc66a
skiiing73 <corentindegouey@gmail.com>
2025-02-13 10:40:21 +0100

    ajout header

--- hackaton-project/src/app/app.component.ts
+++ hackaton-project/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hackaton-project';
+  showHeader: boolean = true; // Default to show header
 }
